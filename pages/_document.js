3b005b11f88ce9690d103509ee788182edcbcd6e
Jose <josecarlosavareza167@gmail.com>
2026-06-06 22:43:16 +0200

    feat: PWA manifest link + Apple meta

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,12 @@ export default function Document() {
       <Head>
         <link rel="icon" href="/favicon.ico" type="image/png"/>
         <link rel="shortcut icon" href="/favicon.ico" type="image/png"/>
-        <meta name="theme-color" content="#04060e"/>
+        <meta name="theme-color" content="#05080f"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <link rel="apple-touch-icon" href="/astra-icon-192.png"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
+        <meta name="apple-mobile-web-app-title" content="Astra HQ"/>
       </Head>
       <body>
         <Main />
